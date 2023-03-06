624bcb8940962fc5282699ca1a8e924bd461bb6c
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2023-03-06 15:32:01 +0800

    feat: 🎸 example

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <VForm>
+    <VField
+      label="電子信箱"
+      name="email"
+      type="email"
+      rules="required|email"
+    />
+    <ErrorMessage name="email"/>
+  </VForm>
+</template>
+
+<style scoped>
+
+</style>
